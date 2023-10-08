a27e6b51f286ef7eaaac7eb4e26ee8b6db20a5c5
Karl Trout <karl.ctr.trout@faa.gov>
2023-10-08 14:37:47 -0400

    finished ( might want to add a default style?)

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import getopt
 import re
 import sys
+
+import geopandas as gpd
 import pandas as pd
+from pandas.core.groupby import DataFrameGroupBy
+from shapely import Polygon
 
 
 def readfile(file):
     filedata = pd.read_csv(file)
     filedata.drop(columns=['Lower Altitude', 'Lat Lon Neighbor'], inplace=True)
-    artccs = filedata.groupby("Facility ID")
-    for name in artccs.groups:
-        print(name)
-    zny = artccs.get_group("ZNY")
-    print(zny)
+    filedata = (filedata
+                .rename(columns={"Facility ID": "artcc", "Upper Altitude": "altitude",
+                                 "Latitude": "latitude", "Longitude": "longitude"}))
+    filedata['latitude'] = filedata['latitude'].apply(dms2dd)
+    filedata['longitude'] = filedata['longitude'].apply(dms2dd)
+    filedata.dropna(axis=0, inplace=True)
+    gdf = gpd.GeoDataFrame(
+        filedata, geometry=gpd.points_from_xy(filedata.longitude, filedata.latitude), crs="EPSG:4326"
+    )
+    return gdf
 
 
 def dms2dd(dms: str):
+    if not isinstance(dms, str):
+        return float('NaN')
+
     pattern = r"(\d{3})(\d{2})(\d{2})(\d{2})([N,S,E,W]{1})" if (len(dms) == 10) \
         else r"(\d{2})(\d{2})(\d{2})(\d{2})([N,S,E,W]{1})"
     results = re.fullmatch(pattern, dms)
     if results is None:
         return float('NaN')
     degrees, minutes, seconds, decisecond, direction = re.search(pattern, dms).groups()
-    dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60) + float(decisecond)*.01
+    dd = float(degrees) + float(minutes)/60 + (float(seconds) + float(decisecond)*.01) /(60*60)
     if direction in ('S', 'W'):
         dd *= -1
     return dd
@@ -38,7 +50,12 @@ def main(argv):
             inputfile = arg
     print('Input file is ', inputfile)
 
-    readfile(inputfile)
+    artccs = readfile(inputfile)
+    artccs = (artccs.groupby(artccs.artcc)['geometry']
+              .apply(lambda x: Polygon(x.tolist())))
+    artccs.crs = 'epsg:4326'
+    print(type(artccs))
+    artccs.to_file('artccs.shp', driver='ESRI Shapefile')
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -6,9 +6,13 @@ import math
 
 class Test(TestCase):
     def test_dms2dd(self):
-        result = main.dms2dd("42330000N")
-        self.assertEqual(result, 42.55)
+        result = main.dms2dd("38104763N")
+        self.assertAlmostEqual(result, 38.179897, 3)
         result = main.dms2dd("077060000W")
-        self.assertEqual(result, -77.1)
+        self.assertAlmostEqual(result, -77.1, 3)
+        result = main.dms2dd("077060010W")
+        self.assertAlmostEqual(result, -77.1, 3 )
         result = main.dms2dd("xyz")
         assert(math.isnan(result))
+        result = main.dms2dd(42.0)
+        assert math.isnan(result)
